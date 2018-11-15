1492ba163501332e212ea1cd7c5166ccd689d466
zsx0728 <zsx0728@163.com>
2018-11-15 17:45:05 +0800

    Modify the sysctl.conf to optimize the kernel

---
+++ KernelOptimization.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#for Kernel optimization
+
+#backup the file
+/bin/cp /etc/sysctl.conf /etc/sysctl.conf.`date +%F_%T`
+
+cat >> /etc/sysctl.conf<<EOF
+net.ipv4.tcp_fintimeout = 2
+net.ipv4.tcp_tw_reuse = 1
+net.ipv4.tcp_tw_recycle = 1
+net.ipv4.tcp_syncookies = 1
+net.ipv4.tcp_keepalive_time = 600
+
+net.ipv4.ip_local_port_range = 4000 65000
+net.ipv4.tcp_max_syn_backlog = 16384
+net.ipv4.tcp_max_tw_buckets = 36000
+net.ipv4.route.gc_timeout =100
+net.ipv4.tcp_syn_retries = 1
+net.ipv4.tcp_synack_retries = 1
+net.core.somaxconn = 16384
+net.core.netdev_max_backlog = 16384
+net.ipv4.tcp_max_orphans = 16384
+#end
+EOF
+sysctl -p &> /dev/null
