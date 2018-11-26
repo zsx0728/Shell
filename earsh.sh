b958573746991eed9ac63411999c0fd482d5db0f
zsx0728 <35521209+zsx0728@users.noreply.github.com>
2018-11-26 11:48:37 +0800

    like the dustbin in windows
    like the dustbin in windows

--- earsh.sh
+++ earsh.sh
@@ -3,16 +3,16 @@
 #if i want,i can get back it.
 
 #test .temp directory
-if [ -d `pwd`/.temp ];then
+if [ -d `echo $HOME`/.temp ];then
     echo "the dustbin is exist." 
 else
     echo "the dustbin isn't exist"
-    mkdir `pwd`/.temp
+    mkdir `echo $HOME`/.temp
 fi
 
 #test file/directory exist or not
 if [ -d `pwd`/$1 ] || [ -f `pwd`/$1 ];then
-    mv `pwd`/$1 `pwd`/.temp
+    mv `pwd`/$1 `echo $HOME`/.temp
 else
     echo "your input doesn't exist."
 fi
