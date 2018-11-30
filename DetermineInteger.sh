ea3f2cc3d9eb838de62652244cdde5e4fc805682
zsx0728 <35521209+zsx0728@users.noreply.github.com>
2018-11-30 15:56:11 +0800

    Update DetermineInteger.sh

--- DetermineInteger.sh
+++ DetermineInteger.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #read something,and determine whether the input is an integer. 
 
+function exprTest()
+{
+expr $1 + 0 &>/dev/null && echo "your input is an integer" || echo "your input isn't an integer "
+return 0
+}
 read -p "please input something,I will tell you whether it is a integer:" Num
-expr $Num + 0 &>/dev/null && echo "your input is an integer" || echo "your input isn't an integer "
+exprTest $Num
