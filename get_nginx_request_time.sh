9335464970cb50278ae5c47cdc474a36d5727115
zsx0728 <35521209+zsx0728@users.noreply.github.com>
2020-05-11 11:39:07 +0800

    Create get_nginx_request_time.sh
    提取request_time大于1的nginx日志，其中，request_time必须为每行日志的最后一项

---
+++ get_nginx_request_time.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#提取request_time大于1的nginx日志，其中，request_time必须为每行日志的最后一项，如：
+#-^-^09/May/2020:03:18:05 +0800^200^HEAD /favicon.ico HTTP/1.0^0^-^-^222.186.132.82^0.001
+
+if [ -f final.txt ]
+then
+    rm -f final.txt
+fi
+
+cat $1|while read line
+do
+    request_time=`echo $line|awk -F'^' '{print $10}'`
+    #echo ${request_time:0:1}
+    if [ ${request_time:0:1} != 0 ]
+    then
+       echo $line >> final.txt 
+    fi
+done
