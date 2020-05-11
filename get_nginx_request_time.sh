7496b0516a4c07a3cdfda83fa7a467fd6e7b714f
zsx0728 <35521209+zsx0728@users.noreply.github.com>
2020-05-11 11:40:46 +0800

    Update get_nginx_request_time.sh
    提取request_time大于1的nginx日志，其中，request_time必须为每行日志的最后一项

--- get_nginx_request_time.sh
+++ get_nginx_request_time.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #提取request_time大于1的nginx日志，其中，request_time必须为每行日志的最后一项，如：
 #-^-^09/May/2020:03:18:05 +0800^200^HEAD /favicon.ico HTTP/1.0^0^-^-^222.186.132.82^0.001
+#使用方法：sh get_nginx_request_time.sh nginx_log_name
 
 if [ -f final.txt ]
 then
